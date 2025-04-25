2de7d1217a5776465a0c382c8f19fbdf050ac7ea
tejitha-pukkalla <tejithapukkalla@gmail.com>
2025-04-25 10:37:28 +0530

    about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import IndexPage from "./pages/index";
 
 const App = () => {
   return <IndexPage />;
+  <script src="https://cdn.tailwindcss.com/?plugins=forms,typography,aspect-ratio"></script>
 };
 
 export default App;
